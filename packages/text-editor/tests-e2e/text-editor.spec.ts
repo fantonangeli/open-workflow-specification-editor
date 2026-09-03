37fc5c61f19abad7a83a11c61792a5c3874a4aed
fantonangeli <fabrizio.antonangeli@gmail.com>
2026-09-03 14:57:00 +0200

    Add text-editor package
    Signed-off-by: fantonangeli <fabrizio.antonangeli@gmail.com>

---
+++ packages/text-editor/tests-e2e/text-editor.spec.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2021-Present The Open Workflow Specification Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { test, expect } from "@playwright/test";
+
+test("Monaco editor renders and is interactive", async ({ page }) => {
+  await page.goto("/iframe.html?id=text-editor--json-editor");
+
+  // Monaco wraps its content in a div with class .monaco-editor
+  const monacoContainer = page.locator(".monaco-editor").first();
+  await expect(monacoContainer).toBeVisible();
+
+  // Monaco always renders an internal textarea for keyboard interaction
+  const monacoTextarea = page.locator(".monaco-editor textarea").first();
+  await expect(monacoTextarea).toBeAttached();
+});
