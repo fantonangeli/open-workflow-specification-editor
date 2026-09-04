f22e3e90832ab64f74a8e853ec9a4fb585332da3
GitHub Pages deploy <github-pages-deploy@users.noreply.github.com>
2026-09-04 11:20:37 +0200

    Deploy text-editor Storybook preview

---
+++ issue-332/assets/Welcome-C4bKFzU0.js
@@ -0,0 +1,20 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t}from"./iframe-BRLLjB-0.js";import{i as n,r}from"./react-DpTkBzf4.js";import{a as i,o as a}from"./blocks-CSFTy6Ya.js";function o(e){let t={a:`a`,code:`code`,h1:`h1`,h2:`h2`,li:`li`,p:`p`,pre:`pre`,strong:`strong`,ul:`ul`,...n(),...e.components};return(0,c.jsxs)(c.Fragment,{children:[`
+`,`
+`,(0,c.jsx)(i,{title:`Introduction/Welcome`}),`
+`,(0,c.jsx)(t.h1,{id:`welcome-to-the-text-editor`,children:`Welcome to the Text Editor`}),`
+`,(0,c.jsxs)(t.p,{children:[`The `,(0,c.jsx)(t.strong,{children:`Open Workflow Text Editor`}),` is an embeddable React component for editing `,(0,c.jsx)(t.a,{href:`https://open-workflow-specification.org`,rel:`nofollow`,children:`Open Workflow Specification`}),` documents.`]}),`
+`,(0,c.jsxs)(t.p,{children:[`It is powered by `,(0,c.jsx)(t.a,{href:`https://github.com/microsoft/monaco-editor`,rel:`nofollow`,children:`Monaco Editor`}),` and supports:`]}),`
+`,(0,c.jsxs)(t.ul,{children:[`
+`,(0,c.jsx)(t.li,{children:`JSON and YAML syntax highlighting;`}),`
+`,(0,c.jsx)(t.li,{children:`editable and read-only modes;`}),`
+`,(0,c.jsx)(t.li,{children:`controlled content updates;`}),`
+`,(0,c.jsx)(t.li,{children:`Monaco language features such as completions and diagnostics.`}),`
+`]}),`
+`,(0,c.jsx)(t.h2,{id:`get-started`,children:`Get started`}),`
+`,(0,c.jsxs)(t.p,{children:[`Explore the stories in the `,(0,c.jsx)(t.strong,{children:`Features`}),` section to see the editor in action.`]}),`
+`,(0,c.jsx)(t.p,{children:`The editor fills its parent container, which must have a non-zero height:`}),`
+`,(0,c.jsx)(t.pre,{children:(0,c.jsx)(t.code,{className:`language-tsx`,children:`<div style={{ height: "400px" }}>
+  <TextEditor content={content} language="yaml" onContentChange={setContent} />
+</div>
+`})}),`
+`,(0,c.jsxs)(t.p,{children:[`For more information, visit the `,(0,c.jsx)(t.a,{href:`https://github.com/open-workflow-specification/editor`,rel:`nofollow`,children:`GitHub repository`}),`.`]})]})}function s(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,c.jsx)(t,{...e,children:(0,c.jsx)(o,{...e})}):o(e)}var c;function l(){return(l=e((()=>{c=t(),r(),a()})))()}l();export{s as default};
---
+++ issue-332/assets/iframe-Cqrtgrqb.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-BRLLjB-0.js";e();
